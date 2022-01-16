805c470f9899b0a9850aa3890672d3b0d8d9d323
Abdoul Sadikou <sadikouhak@gmail.com>
2022-01-16 16:43:51 -0500

    app left and right done

--- covid_tracker/src/App.js
+++ covid_tracker/src/App.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react'
-import {MenuItem, FormControl, Select} from '@material-ui/core'
+import {MenuItem, FormControl, Select, Card, CardContent} from '@material-ui/core'
 import './App.css';
 import InfoBox from './InfoBox'
+import Map from './Map'
 
 function App() {
 
@@ -37,40 +38,49 @@ useEffect(() => {
   return (
     <div className="App">
 
-      <div className="app__header">
-        <h1>COVID-19 TRACKER</h1>
-        <FormControl className="app_dropdown">
-          <Select variant="outlined" value={country} onChange={onCountryChange} >
+      <div className='app__left'>
 
-            {/*Loop through all the country and display them */} 
-            <MenuItem value="worlwide">Worlwide</MenuItem>
-            {
-              countries.map((country)=>(
-                <MenuItem value={country.value}>{country.name}</MenuItem>
-              ))
-            }           
-          </Select>
-        </FormControl>
-      </div>
+
+        {/* Header*/}
+        { /*Title + select input dropdown field */}
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app_dropdown">
+            <Select variant="outlined" value={country} onChange={onCountryChange} >
+
+              {/*Loop through all the country and display them */} 
+              <MenuItem value="worlwide">Worlwide</MenuItem>
+              {
+                countries.map((country)=>(
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+              }           
+            </Select>
+          </FormControl>
+        </div>
       
-
-      <div className='app__stats'>
+        {/* Infoboxs*/}
+        {/* Infoboxs*/}
+        {/* Infoboxs*/}
+        <div className='app__stats'>
           <InfoBox title='Corona Cases'  cases={1234} total={53000} />
           <InfoBox title='Recovered'  cases={1234} total={4000} />
           <InfoBox title='Deaths'  cases={1234} total={4000000} />
+        </div>
+        {/*Map*/}
+        <Map />
+
       </div>
-      {/* Header*/}
-      { /*Title + select input dropdown field */}
-
-      {/* Infoboxs*/}
-      {/* Infoboxs*/}
-      {/* Infoboxs*/}
-
-      {/* Table*/}
-      {/* Graph*/}
-
-      {/*Map*/}
       
+      <Card className='app__right'>
+        {/* Table*/}
+        {/* Graph*/}
+        <CardContent>
+           <h3>Live cases by Country</h3>
+           <h3>Worldwide new cases</h3>   
+        </CardContent>
+      </Card>
+    
     </div>
   );
 }
---
+++ covid_tracker/src/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Map = () => {
+    return (
+        <div className='Map'>
+            <h1> I am a map</h1>
+        </div>
+    )
+}
+
+export default Map
